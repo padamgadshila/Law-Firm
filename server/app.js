ad565eec455a779524ec042243e36550e7b0a42e
Padam Gadshila <padamgadshila17@gmail.com>
2025-01-12 12:04:23 +0530

    Added notFound and ErrorHandler

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { notFound, errorHandler } from "./Middlewares/error.middleware.js";
 const app = express();
 
 //use
 app.use(cors());
 app.use(morgan("common"));
 app.use(express.json());
+// app.use(notFound());
+// app.use(errorHandler());
 
 export default app;
